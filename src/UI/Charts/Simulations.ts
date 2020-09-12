2b8410c1aa72044d7dab6893f607ddf5cb23210a
FaultyIce <m@aqu.me.uk>
2020-09-12 13:03:24 +0100

    remove old code

--- src/UI/Charts/Simulations.ts
+++ src/UI/Charts/Simulations.ts
@@ -21,18 +21,11 @@ export function ProbabilitySimulation(method: () => number, numberOfSimulation =
 function simulate(numberOfSimulation: number, method: () => number) {
   const results = new Map<number, number>();
   for (let index = 0; index < numberOfSimulation; index++) {
-    const r = method();
-    results.set(r, (results.get(r) || 0) + 1);
+    const result = method();
+    results.set(result, (results.get(result) ?? 0) + 1);
   }
   return results;
 }
-
-// function ToFrequency(results: number[]) {
-//   return results.reduce((acc, value) => {
-//     acc.set(value, (acc.get(value) || 0) + 1);
-//     return acc;
-//   }, new Map<number, number>());
-// }
 
 function ToChartData(results: Map<number, number>) {
   return [...results.entries()].map(([value, freq]) => ({ x: value, y: freq })).sort((a, b) => a.x - b.x);
